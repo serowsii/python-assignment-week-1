9d7fc7c4227141569f7e81470970d06a90199e44
serowsii <kiungachelsea008@gmail.com>
2024-11-05 20:09:37 +0300

    user input

---
+++ user_input.py
@@ -0,0 +1,13 @@
+name = input("please enter your name : ")
+age = input("please enter your age : ")
+location = input("please enter your current location : ")
+
+#lets print a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
+
+#outome  after running : 
+#please enter your name : chelsea
+#please enter your age : 23
+#please enter your current location : nakuru
+#Hello chelsea, you are 23 years old and live in nakuru.
